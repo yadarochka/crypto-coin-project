02a6259bc4c31c2a3c308812e996b559e90c0199
yadarochka <66113821+yadarochka@users.noreply.github.com>
2022-09-02 02:30:24 +0500

    добавлен mobx и хук useLocalStore

---
+++ src/utils/useLocalStore.ts
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from "react";
+
+export interface ILocalStore {
+  destroy(): void;
+}
+
+export const useLocalStore = <T extends ILocalStore>(creator: () => T): T => {
+  const container = useRef<T | null>(null);
+  if (container.current === null) container.current = creator();
+
+  useEffect(() => {
+    return () => container.current?.destroy();
+  });
+
+  return container.current;
+};
