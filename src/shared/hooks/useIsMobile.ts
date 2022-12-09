f35ea873672d557348d29fd54d5d7afb6453bacf
yadarochka <66113821+yadarochka@users.noreply.github.com>
2022-12-09 23:32:41 +0500

    Create useIsMobile.ts

---
+++ src/shared/hooks/useIsMobile.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export const useIsMobile = () => {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+
+  return isMobile;
+};
