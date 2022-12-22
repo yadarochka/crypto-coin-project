f6349c9e8fef04b4286b9bd5c1bacf292063e85d
yadarochka <66113821+yadarochka@users.noreply.github.com>
2022-12-22 15:54:00 +0500

    Update IncreaseOrDecreaseProps.test.tsx

--- src/components/UI/IncreaseOrDecrease/IncreaseOrDecreaseProps.test.tsx
+++ src/components/UI/IncreaseOrDecrease/IncreaseOrDecreaseProps.test.tsx
@@ -1,6 +1,5 @@
 import "@testing-library/jest-dom";
-import { getByText, render, screen } from "@testing-library/react";
-import "jest-styled-components";
+import { render, screen } from "@testing-library/react";
 
 import { IncreaseOrDecrease } from "./IncreaseOrDecrease";
 
