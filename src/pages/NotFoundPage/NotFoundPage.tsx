99ed947c8ffeaaf3f33d7375fbc3143143af7bbc
yadarochka <66113821+yadarochka@users.noreply.github.com>
2022-12-24 19:27:32 +0500

    Update NotFoundPage.tsx

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,16 +1,10 @@
 import { NotFound } from "widgets/NotFound";
 
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
-import Button from "../../components/UI/Button";
 import styles from "./NotFoundPage.module.scss";
 
 export const NotFoundPage = () => {
-  const navigate = useNavigate();
-
-  const handlerButtonClick = () => navigate("/");
-
   return (
     <div className={styles["not-found"]}>
       <NotFound />
